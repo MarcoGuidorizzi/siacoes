70ca30655f080c9ce88298bc0b0797cbc84630d7
MarcoGuidorizzi <guidorizzi68@gmail.com>
2020-09-01 02:00:12 -0300

    tryResourceClose no BugReportDAO para minimizar o código e garantir o fechamento das conexões

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/BugReportDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/BugReportDAO.java
@@ -15,81 +15,66 @@ import br.edu.utfpr.dv.siacoes.model.Module;
 import br.edu.utfpr.dv.siacoes.model.User;
 
 public class BugReportDAO {
-	
-	public BugReport findById(int id) throws SQLException{
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.prepareStatement("SELECT bugreport.*, \"user\".name " + 
+
+    public BugReport findById(int id) throws SQLException{
+
+		// Utilizando o método de tryResourceClose para minimizar o código e garantir o fechamento das conexões
+        // Fonte: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+		String query = "SELECT bugreport.*, \"user\".name " + 
 				"FROM bugreport INNER JOIN \"user\" ON \"user\".idUser=bugreport.idUser " +
-				"WHERE idBugReport = ?");
-		
-			stmt.setInt(1, id);
-			
-			rs = stmt.executeQuery();
-			
-			if(rs.next()){
-				return this.loadObject(rs);
-			}else{
-				return null;
-			}
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
+				"WHERE idBugReport = ?";
+		try (Connection conn = ConnectionDAO.getInstance().getConnection();
+                PreparedStatement stmt = conn.prepareStatement(query);) {
+            stmt.setInt(1, id);
+        
+            try(ResultSet rs = stmt.executeQuery();){
+                if(rs.next()){
+                    return this.loadObject(rs);
+                }else{
+                    return null;
+                }
+            }
+
 		}
 	}
 	
 	public List<BugReport> listAll() throws SQLException{
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
-			
-			rs = stmt.executeQuery("SELECT bugreport.*, \"user\".name " +
+
+        // Utilizando o método de tryResourceClose para minimizar o código e garantir o fechamento das conexões
+        // Fonte: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+		String query = "SELECT bugreport.*, \"user\".name " +
 					"FROM bugreport INNER JOIN \"user\" ON \"user\".idUser=bugreport.idUser " +
-					"ORDER BY status, reportdate");
+					"ORDER BY status, reportdate";
+		try (Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(query);) {
 			List<BugReport> list = new ArrayList<BugReport>();
-			
+
 			while(rs.next()){
-				list.add(this.loadObject(rs));
+                list.add(this.loadObject(rs));
 			}
 			
 			return list;
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
 	}
 	
 	public int save(BugReport bug) throws SQLException{
 		boolean insert = (bug.getIdBugReport() == 0);
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
+
+        // Colocando INSERT e UPDATE em funções separadas para melhor entendimento do código
+		if (insert) {
+			return insertBugReport(bug);
+		} else {
+			return updateBugReport(bug);
+		}
+	}
+
+	// Criando método privado para INSERT
+    private int insertBugReport(BugReport bug) throws SQLException{
+        String sql = "INSERT INTO bugreport(idUser, module, title, description, reportDate, type, status, statusDate, statusDescription) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			
-			if(insert){
-				stmt = conn.prepareStatement("INSERT INTO bugreport(idUser, module, title, description, reportDate, type, status, statusDate, statusDescription) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-			}else{
-				stmt = conn.prepareStatement("UPDATE bugreport SET idUser=?, module=?, title=?, description=?, reportDate=?, type=?, status=?, statusDate=?, statusDescription=? WHERE idBugReport=?");
-			}
-			
+		try(Connection conn = ConnectionDAO.getInstance().getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 			stmt.setInt(1, bug.getUser().getIdUser());
 			stmt.setInt(2, bug.getModule().getValue());
 			stmt.setString(3, bug.getTitle());
@@ -104,30 +89,55 @@ public class BugReportDAO {
 			}
 			stmt.setString(9, bug.getStatusDescription());
 			
-			if(!insert){
-				stmt.setInt(10, bug.getIdBugReport());
+			// Usando executeUpdate para garantir o INSERT
+			// o método executeUpdate é destinado para INSERT, UPDATE e DELETE
+			int rows = stmt.executeUpdate();
+			if(rows == 0){
+                throw new SQLException("Falha ao inserir no banco.");
 			}
-			
-			stmt.execute();
-			
-			if(insert){
-				rs = stmt.getGeneratedKeys();
-				
-				if(rs.next()){
-					bug.setIdBugReport(rs.getInt(1));
-				}
-			}
-			
-			return bug.getIdBugReport();
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
+
+			try(ResultSet rs = stmt.getGeneratedKeys()){
+                if(rs.next()){
+                    bug.setIdBugReport(rs.getInt(1));
+                }
+
+                return bug.getIdBugReport();
+            }
 		}
 	}
+        
+	// Criando método privado para UPDATE
+    private int updateBugReport(BugReport bug) throws SQLException{
+        String sql = "UPDATE bugreport SET idUser=?, module=?, title=?, description=?, reportDate=?, type=?, status=?, statusDate=?, statusDescription=? WHERE idBugReport=?";
+		
+		try(Connection conn = ConnectionDAO.getInstance().getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);){
+			stmt.setInt(1, bug.getUser().getIdUser());
+			stmt.setInt(2, bug.getModule().getValue());
+			stmt.setString(3, bug.getTitle());
+			stmt.setString(4, bug.getDescription());
+			stmt.setDate(5, new java.sql.Date(bug.getReportDate().getTime()));
+			stmt.setInt(6, bug.getType().getValue());
+			stmt.setInt(7, bug.getStatus().getValue());
+			if(bug.getStatus() == BugStatus.REPORTED){
+				stmt.setNull(8, Types.DATE);
+			}else{
+				stmt.setDate(8, new java.sql.Date(bug.getStatusDate().getTime()));
+			}
+			stmt.setString(9, bug.getStatusDescription());
+			stmt.setInt(10, bug.getIdBugReport());
+			
+			// Usando executeUpdate para garantir o UPDATE
+			// o método executeUpdate é destinado para INSERT, UPDATE e DELETE
+			int rows = stmt.executeUpdate();
+			if(rows == 0){
+				throw new SQLException("Falha ao fazer update no banco, id não encontrado.");
+			}
+
+			return bug.getIdBugReport();
+		}
+	}
+
 	
 	private BugReport loadObject(ResultSet rs) throws SQLException{
 		BugReport bug = new BugReport();
