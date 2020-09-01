d25a64f49d2feb683a173b0aa965ae8cde5940e3
MarcoGuidorizzi <guidorizzi68@gmail.com>
2020-09-01 01:06:27 -0300

    tryResourceClose para minimizar o código

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/ActivityUnitDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/ActivityUnitDAO.java
@@ -14,106 +14,108 @@ import br.edu.utfpr.dv.siacoes.model.ActivityUnit;
 public class ActivityUnitDAO {
 	
 	public List<ActivityUnit> listAll() throws SQLException{
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
-		
-			rs = stmt.executeQuery("SELECT * FROM activityunit ORDER BY description");
-			
+        // Utilizando o método de tryResourceClose para minimizar o código e garantir o fechamento das conexões
+        // Fonte: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+        String query = "SELECT * FROM activityunit ORDER BY description";
+
+		try (Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(query);) {
 			List<ActivityUnit> list = new ArrayList<ActivityUnit>();
-			
-			while(rs.next()){
+
+			while (rs.next()) {
 				list.add(this.loadObject(rs));
 			}
-			
+
 			return list;
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
+
 		}
 	}
 	
 	public ActivityUnit findById(int id) throws SQLException{
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.prepareStatement("SELECT * FROM activityunit WHERE idActivityUnit=?");
-		
+        // Utilizando o método de tryResourceClose para minimizar o código e garantir o fechamento das conexões
+        // Fonte: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+		String query = "SELECT * FROM activityunit WHERE idActivityUnit=?";
+
+		try (Connection conn = ConnectionDAO.getInstance().getConnection();
+				PreparedStatement stmt = conn.prepareStatement(query);) {
 			stmt.setInt(1, id);
-			
-			rs = stmt.executeQuery();
-			
-			if(rs.next()){
-				return this.loadObject(rs);
-			}else{
-				return null;
+
+			try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) {
+					return this.loadObject(rs);
+				} else {
+					return null;
+				}
 			}
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
 	}
 	
-	public int save(int idUser, ActivityUnit unit) throws SQLException{
+	public int save(int idUser, ActivityUnit unit) throws SQLException {
 		boolean insert = (unit.getIdActivityUnit() == 0);
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			
-			if(insert){
-				stmt = conn.prepareStatement("INSERT INTO activityunit(description, fillAmount, amountDescription) VALUES(?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-			}else{
-				stmt = conn.prepareStatement("UPDATE activityunit SET description=?, fillAmount=?, amountDescription=? WHERE idActivityUnit=?");
-			}
-			
+
+	    // Colocando INSERT e UPDATE em funções separadas para melhor entendimento do código
+		if (insert) {
+			return insertActivityUnit(idUser, unit);
+		} else {
+			return updateActivityUnit(idUser, unit);
+		}
+	}
+
+	// Criando método privado para INSERT
+	private int insertActivityUnit(int idUser, ActivityUnit unit) throws SQLException {
+		String sql = "INSERT INTO activityunit(description, fillAmount, amountDescription) VALUES(?, ?, ?)";
+
+        // Utilizando o método de tryResourceClose para minimizar o código e garantir o fechamento das conexões
+        // Fonte: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+		try (Connection conn = ConnectionDAO.getInstance().getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
 			stmt.setString(1, unit.getDescription());
 			stmt.setInt(2, (unit.isFillAmount() ? 1 : 0));
 			stmt.setString(3, unit.getAmountDescription());
-			
-			if(!insert){
-				stmt.setInt(4, unit.getIdActivityUnit());
-			}
-			
-			stmt.execute();
-			
-			if(insert){
-				rs = stmt.getGeneratedKeys();
-				
-				if(rs.next()){
+
+            // Usanddo executeUpdate para garantir o INSERT
+            // o método executeUpdate é destinado para INSERT, UPDATE e DELETE
+            int rows = stmt.executeUpdate();
+            if(rows == 0){
+                throw new SQLException("Falha ao inserir no banco.");
+            }
+
+			try (ResultSet rs = stmt.getGeneratedKeys()) {
+				if (rs.next()) {
 					unit.setIdActivityUnit(rs.getInt(1));
 				}
-				
+
 				new UpdateEvent(conn).registerInsert(idUser, unit);
-			} else {
-				new UpdateEvent(conn).registerUpdate(idUser, unit);
+
+				return unit.getIdActivityUnit();
 			}
-			
+		}
+	}
+
+	// Criando método privado para UPDATE
+	private int updateActivityUnit(int idUser, ActivityUnit unit) throws SQLException {
+		String sql = "UPDATE activityunit SET description=?, fillAmount=?, amountDescription=? WHERE idActivityUnit=?";
+
+        // Utilizando o método de tryResourceClose para minimizar o código e garantir o fechamento das conexões
+        // Fonte: https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+		try (Connection conn = ConnectionDAO.getInstance().getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql);) {
+			stmt.setString(1, unit.getDescription());
+			stmt.setInt(2, (unit.isFillAmount() ? 1 : 0));
+			stmt.setString(3, unit.getAmountDescription());
+			stmt.setInt(4, unit.getIdActivityUnit());
+
+            // Usando executeUpdate para garantir o UPDATE
+            // o método executeUpdate é destinado para INSERT, UPDATE e DELETE
+            int rows = stmt.executeUpdate();
+            if(rows == 0){
+                throw new SQLException("Falha ao fazer update no banco, id não encontrado.");
+            }
+
+			new UpdateEvent(conn).registerUpdate(idUser, unit);
+
 			return unit.getIdActivityUnit();
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
 	}
 	
